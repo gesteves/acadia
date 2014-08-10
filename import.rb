b942e4808afdd9c6e33f91a005c14b89da604aa5
Guillermo Esteves <g@gesteves.com>
2014-08-10 17:56:36 -0400

    Expand links in tweets

--- import.rb
+++ import.rb
@@ -17,12 +17,70 @@ def get_tweets
     consumer = OAuth::Consumer.new(consumer_key, consumer_secret, { site: "http://api.twitter.com" })
     access_token = OAuth::AccessToken.new(consumer, access_token, access_token_secret)
     response = access_token.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}&exclude_replies=true&include_rts=false")
-    tweets = JSON.parse(response.body).slice(0, count).to_json
+    tweets = JSON.parse(response.body).slice(0, count).map!{ |t| expand_tweet(t) }.to_json
     File.open("data/twitter.json","w"){ |f| f << tweets }
   rescue OAuth::Error => e
     puts e
   end
 end
+
+# Horrible method to expand tweet entities (urls, hashtags, mentions, etc.) in tweets
+def expand_tweet(tweet)
+  text = tweet["text"]
+  expanded_text = []
+  # Put all the entities into an array and sort them by starting index
+  entities = []
+  entities += tweet["entities"]["urls"] unless tweet["entities"]["urls"].nil?
+  entities += tweet["entities"]["user_mentions"] unless tweet["entities"]["user_mentions"].nil?
+  entities += tweet["entities"]["media"] unless tweet["entities"]["media"].nil?
+  entities += tweet["entities"]["hashtags"] unless tweet["entities"]["hashtags"].nil?
+  entities.sort!{ |a,b| a["indices"].first <=> b["indices"].first } if entities.size > 1
+  entities.each_with_index do |e, i|
+    # If it's the first entity, start by putting the text from the beginning of the tweet
+    # to the start of the entity in the placeholder array
+    if i == 0
+      end_index = e["indices"].first - 1
+      expanded_text << text[0..end_index]
+    end
+
+    # Now expand the entity link and place it in the array
+    expanded_text << expand_tweet_entity(e)
+
+    # If I'm at the last entity, place the rest of the tweet text in the placeholder array
+    if i == entities.size - 1
+      start_index = e["indices"].last
+      end_index = text.size - 1
+      expanded_text << text[start_index..end_index]
+    # If not, place the text between the end of the current entity and the start of the next one
+    else
+      start_index = e["indices"].last
+      end_index = entities[i + 1]["indices"].first - 1
+      expanded_text << text[start_index..end_index]
+    end
+  end
+  
+  # Now join the placeholder array into a string and put it in the tweet object
+  tweet["expanded_text"] = expanded_text.join
+
+  # Return the tweet with the new expanded text. Phew.
+  tweet
+end
+
+# Spit out the correct link tag for each type of entity
+def expand_tweet_entity(e)
+  if !e["display_url"].nil? && !e["expanded_url"].nil?
+    link_to(e["display_url"], e["expanded_url"])
+  elsif !e["screen_name"].nil?
+    link_to("@#{e["screen_name"]}", "https://twitter.com/#{e["screen_name"]}")
+  elsif !e["text"].nil?
+    link_to("##{e["text"]}", "https://twitter.com/hashtag/#{e["text"]}")
+  end
+end
+
+def link_to(text, url)
+  "<a href=\"#{url}\">#{text}</a>"
+end
+
 
 def get_instagram_photos
   begin
