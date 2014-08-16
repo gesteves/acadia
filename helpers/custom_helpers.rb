e4456233e586f1cfb5f98389ddb13ade0af827c9
Guillermo Esteves <g@gesteves.com>
2014-08-15 20:26:01 -0400

    Display Twitter actions and user info

---
+++ helpers/custom_helpers.rb
@@ -0,0 +1,10 @@
+module CustomHelpers
+  require "date"
+  require "tzinfo"
+
+  def formatted_date_in_eastern(date_string, format = "%F")
+    tz = TZInfo::Timezone.get("America/New_York")
+    local = tz.utc_to_local(DateTime.parse(date_string))
+    local.strftime(format)
+  end
+end
