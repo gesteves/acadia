609df0625357d703eeb021a06ebb30943113fc71
Guillermo Esteves <g@gesteves.com>
2014-08-17 16:19:16 -0400

    srcset for the win

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -7,4 +7,28 @@ module CustomHelpers
     local = tz.utc_to_local(DateTime.parse(date_string))
     local.strftime(format)
   end
+
+  def photoblog_image_tag(photo)
+    alt = "Latest from my photoblog"
+    src = image_path "photoblog/#{photo.id}_640.jpg"
+    srcset = []
+    sizes = [1280, 930, 810, 640, 624, 512, 465, 405, 312, 256]
+    sizes.each do |size|
+      srcset << "#{image_path("photoblog/#{photo.id}_#{size}.jpg")} #{size}w"
+    end
+    sizes = "(min-width: 1470px) 312px, (min-width: 1120px) 405px, (min-width: 765px) 465px, (min-width: 400px) 640px, 256px"
+    "<img src=\"#{src}\" srcset=\"#{srcset.join(", ")}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
+  end
+
+  def instagram_image_tag(photo)
+    alt = photo.caption.nil? ? "Instagram photo" : photo.caption.text
+    src = image_path "instagram/#{photo.id}_640.jpg"
+    srcset = []
+    sizes = [640, 384, 280, 242, 192, 188, 154, 141, 121, 94, 77]
+    sizes.each do |size|
+      srcset << "#{image_path("instagram/#{photo.id}_#{size}.jpg")} #{size}w"
+    end
+    sizes = "(min-width: 1470px) 94px, (min-width: 1120px) 121px, (min-width: 765px) 140px, (min-width: 400px) 192px, 77px"
+    "<img src=\"#{src}\" srcset=\"#{srcset.join(", ")}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
+  end
 end
