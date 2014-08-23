f65399685578e316a80a95827e9d7369842cae0a
Guillermo Esteves <g@gesteves.com>
2014-08-23 12:54:30 -0400

    Set up caching and gzip

--- config.rb
+++ config.rb
@@ -1,74 +1,24 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-activate :s3_sync
+activate :gzip
+activate :s3_sync do |s3|
+  s3.prefer_gzip = true
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
+  activate :asset_hash
   activate :relative_assets
+end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+after_configuration do
+  caching_policy "text/html",    max_age: 0, must_revalidate: true
+  caching_policy "image/x-icon", max_age: 60 * 60 * 24 * 365
+  default_caching_policy         max_age: 60 * 60 * 24 * 365
 end
