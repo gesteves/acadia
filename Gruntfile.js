15b050d00e9ed2138f4bed72759647dc15b8cdf0
Guillermo Esteves <g@gesteves.com>
2015-03-12 22:30:59 -0400

    Lint Sass and JS

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'source/javascripts/**/*.js'],
+      options: {
+        esnext: true,
+        node: true,
+        curly: true,
+        eqeqeq: true,
+        indent: 2,
+        quotmark: 'single',
+        unused: true,
+        trailing: true,
+        smarttabs: true,
+        eqnull: true,
+        browser: true,
+        globalstrict: false,
+        globals: {
+          jQuery: true,
+          _: true,
+          Modernizr: true,
+        },
+      }
+    },
+    scsslint: {
+      allFiles: [
+        'source/stylesheets/**/*.scss',
+      ],
+      options: {
+        exclude: 'source/stylesheets/vendors/**/*',
+        config: '.scss-lint.yml',
+        bundleExec: true
+      }
+    },
+    watch: {
+      js: {
+        files: '<%= jshint.files %>',
+        tasks: 'jshint'
+      },
+      sass: {
+        files: ['.scss-lint.yml', 'source/stylesheets/**/*.scss'],
+        tasks: 'scsslint'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-scss-lint');
+
+  grunt.registerTask('default', 'watch');
+};
