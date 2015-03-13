a991cc5daac2289c1029db767ddcf6b1c0c79e60
Guillermo Esteves <g@gesteves.com>
2015-03-13 19:40:28 -0400

    Init livereload only in dev environment

--- config.rb
+++ config.rb
@@ -9,13 +9,16 @@ set :port, 4567
 activate :gzip
 activate :autoprefixer
 activate :dotenv
-activate :livereload
 activate :s3_sync do |s3|
   s3.prefer_gzip           = true
   s3.bucket                = ENV["AWS_BUCKET"]
   s3.region                = ENV["AWS_REGION"]
   s3.aws_access_key_id     = ENV["AWS_ACCESS_KEY_ID"]
   s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY_ID"]
+end
+
+configure :development do
+  activate :livereload
 end
 
 # Build-specific configuration
