7dbe0eca57adfb15f20d10bbdfe10b59682f8b1a
Guillermo Esteves <g@gesteves.com>
2015-07-03 13:12:53 -0400

    Make photoblog tags optional

--- lib/import/photoblog.rb
+++ lib/import/photoblog.rb
@@ -2,14 +2,15 @@ require 'httparty'
 
 module Import
   class Photoblog
-    def initialize(photo_url, photo_tag, photo_count)
+    def initialize(photo_url, photo_count, photo_tag = nil)
       @photo_url    = photo_url
       @photo_tag    = photo_tag
       @photo_count  = photo_count
     end
 
     def get_photos
-      response = HTTParty.get("#{@photo_url}/tagged/#{@photo_tag}/count/#{@photo_count}.json")
+      url = @photo_tag.nil? ? "#{@photo_url}/count/#{@photo_count}.json" : "#{@photo_url}/tagged/#{@photo_tag}/count/#{@photo_count}.json"
+      response = HTTParty.get(url)
       posts = JSON.parse(response.body)['entries']
       File.open('data/photoblog.json','w'){ |f| f << posts.to_json }
     end
