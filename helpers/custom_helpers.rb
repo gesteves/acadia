804b8eee80a54452609bbb729e4d7b50b0f736d6
Guillermo Esteves <guillermo@voxmedia.com>
2015-08-01 10:39:45 -0400

    Add picturefill

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -28,60 +28,54 @@ module CustomHelpers
   def photoblog_image_tag(photo)
     caption = photo.title || "Latest from my photoblog"
     photo_url = photo.photos[0].url
-    src = thumbor_url(photo_url, 693, 693)
     sizes_array = [693, 558, 526, 498, 484, 470, 416, 334, 278, 249, 242, 235]
     srcset = build_srcset(photo_url, sizes_array, true)
     sizes = "(min-width: 1090px) 249px, (min-width: 1000px) calc((100vw - 8rem)/4 - 1px), (min-width: 600px) calc((100vw - 4rem)/3 - 1px), calc((100vw - 4rem)/2 - 1px)"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
   end
 
   def instagram_image_tag(photo)
     caption = photo.caption.nil? ? "Instagram photo" : photo.caption.text
     photo_url = photo.images.standard_resolution.url
-    src = thumbor_url(photo_url, 372, 372)
     sizes_array = [372, 350, 324, 228, 222, 194, 184, 172, 114, 92, 86]
     srcset = build_srcset(photo_url, sizes_array, true)
     sizes = "(min-width: 1360px) 92px, (min-width: 1000px) calc(((100vw - 8rem)/4 - 4rem)/3 - 1px), (min-width: 600px) calc(((100vw - 4rem)/2 - 2rem)/3 - 1px), calc((100vw - 4rem)/3 - 1px)"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
   end
 
   def rdio_image_tag(album)
     alt = album.name
     photo_url = album.icon
-    src = thumbor_url(photo_url, 150)
     sizes_array = [200, 150, 100, 50]
     srcset = build_srcset(photo_url, sizes_array)
     sizes = "50px"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
   end
 
   def twitter_avatar_image_tag(username, name)
     photo_url = username.profile_image_url.sub('_normal', '')
-    src = thumbor_url(photo_url, 150)
     sizes_array = [200, 150, 100, 50]
     srcset = build_srcset(photo_url, sizes_array)
     sizes = "50px"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{name}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{name}\" />"
   end
 
   def untappd_image_tag(beer)
     alt = beer.beer_name
     photo_url = beer.beer_label
-    src = thumbor_url(photo_url, 100)
     sizes_array = [100, 50]
     srcset = build_srcset(photo_url, sizes_array)
     sizes = "50px"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
   end
 
   def goodreads_image_tag(book)
     alt = book.title
     photo_url = book.image
-    src = thumbor_url(photo_url, 150)
     sizes_array = [150, 100, 50]
     srcset = build_srcset(photo_url, sizes_array)
     sizes = "50px"
-    "<img src=\"#{src}\" srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
+    "<img srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{alt}\" />"
   end
 
   def photo_exif(photo)
