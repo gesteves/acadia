295c7b8a58a2147b43a38d2a6de9d8188a37a28c
Guillermo Esteves <g@gesteves.com>
2016-09-18 19:54:23 -0400

    One liner

--- lib/import/untappd.rb
+++ lib/import/untappd.rb
@@ -12,11 +12,7 @@ module Import
     def get_beers
       checkins = JSON.parse(HTTParty.get("https://api.untappd.com/v4/user/info/#{@username}?client_id=#{@client_id}&client_secret=#{@client_secret}").body)['response']['user']['checkins']['items'].uniq{ |b| b['beer']['bid'] }.slice(0, @count)
       checkins.each do |c|
-        url = if c['beer']['beer_label'] =~ /badge-beer-default/
-          c['brewery']['brewery_label']
-        else
-          c['beer']['beer_label']
-        end
+        url = c['beer']['beer_label'] =~ /badge-beer-default/ ? c['brewery']['brewery_label'] : c['beer']['beer_label']
         File.open("source/images/untappd/#{c['beer']['bid']}.jpg",'w'){ |f| f << HTTParty.get(url).body }
       end
       File.open('data/untappd.json','w'){ |f| f << checkins.to_json }
