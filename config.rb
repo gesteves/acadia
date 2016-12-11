1e659abfcfb62447784f5fda86f9b123b3b22b58
Guillermo Esteves <g@gesteves.com>
2016-12-11 13:21:04 -0500

    Remove unused styles

--- config.rb
+++ config.rb
@@ -32,7 +32,7 @@ configure :production do
   activate :sprockets
   activate :gzip
   activate :autoprefixer do |config|
-    config.browsers = ['last 1 version', 'last 3 safari versions', 'last 3 ios versions']
+    config.browsers = ['last 1 version']
     config.inline   = true
   end
   activate :s3_sync do |s3|
