48f5a10451f8378fd45380d885b42e8e42a0b715
Guillermo Esteves <g@gesteves.com>
2016-12-11 11:30:43 -0500

    Use photo captions in alt attrs

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -34,13 +34,14 @@ module CustomHelpers
     srcset.join(', ')
   end
 
-  def photoblog_image_tag(photo, caption = "Latest from my photoblog")
+  def photoblog_image_tag(photo)
+    caption = photo.attributes.caption.nil? ? "Latest from my photoblog" : photo.attributes.caption
     photo_url = image_path "photoblog/#{photo.id}.jpg"
     sizes_array = [558, 498, 249]
     srcset = build_srcset(photo_url, sizes_array)
     src = imgix_url(photo_url, { w: sizes_array.first })
     sizes = "(min-width: 1090px) 249px, (min-width: 1000px) calc((100vw - 8rem)/4 - 1px), (min-width: 600px) calc((100vw - 4rem)/3 - 1px), calc((100vw - 4rem)/2 - 1px)"
-    "<img class=\"js-lazy-load\" data-src=\"#{src}\" data-srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
+    "<img class=\"js-lazy-load\" data-src=\"#{src}\" data-srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" />"
   end
 
   def instagram_image_tag(photo)
@@ -50,7 +51,7 @@ module CustomHelpers
     srcset = build_srcset(photo_url, sizes_array, true)
     src = imgix_url(photo_url, { w: sizes_array.first, square: true })
     sizes = "(min-width: 1360px) 92px, (min-width: 1000px) calc(((100vw - 8rem)/4 - 4rem)/3 - 1px), (min-width: 600px) calc(((100vw - 4rem)/2 - 2rem)/3 - 1px), calc((100vw - 4rem)/3 - 1px)"
-    "<img class=\"js-lazy-load\" data-src=\"#{src}\" data-srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" title=\"#{caption}\" />"
+    "<img class=\"js-lazy-load\" data-src=\"#{src}\" data-srcset=\"#{srcset}\" sizes=\"#{sizes}\" alt=\"#{caption}\" />"
   end
 
   def music_image_tag(album)
