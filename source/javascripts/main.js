ca99b4ff761eac5655db10011443ca3aefb39823
Guillermo Esteves <g@gesteves.com>
2016-12-11 12:18:25 -0500

    Remove lazy load

--- source/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-//= require_tree ./application
-
-if (document.readyState !== 'loading') {
-  Acadia.LazyLoad.init();
-} else {
-  document.addEventListener('DOMContentLoaded', Acadia.LazyLoad.init);
-}
